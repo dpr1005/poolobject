48b173fe5ef43b82682e7e90977d95d6983872b4
Daniel Puente <dpr1005@alu.ubu.es>
2022-02-18 00:00:38 +0100

    Updated testReleaseReusable

--- src/test/ubu/gii/dass/test/c01/NotFreeInstanceExceptionTest.java
+++ src/test/ubu/gii/dass/test/c01/NotFreeInstanceExceptionTest.java
@@ -10,7 +10,8 @@ import java.util.Vector;
 /**
  * NotFreeInstanceException Tester.
  *
- * @author <Authors name>
+ * @author <a href="mailto:dpr1005@alu.ubu.es">Daniel Puente Ramírez</a>
+ * @author <a href="mailto:phf1001@alu.ubu.es">Patricia Hernando Fernandez</a>
  * @version 1.0
  * @since <pre>feb. 17, 2022</pre>
  */
--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -1,57 +1,80 @@
-/**
- * 
- */
 package ubu.gii.dass.test.c01;
-
-import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import ubu.gii.dass.c01.DuplicatedInstanceException;
+import ubu.gii.dass.c01.Reusable;
+import ubu.gii.dass.c01.ReusablePool;
+
+import static org.junit.Assert.assertTrue;
 
 /**
- * @author @dpr1005 @phf1001 - Daniel Puente Ramirez y Patricia Hernando Fernandez
+ * @author <a href="mailto:dpr1005@alu.ubu.es">Daniel Puente Ramírez</a>
+ * @author <a href="mailto:phf1001@alu.ubu.es">Patricia Hernando Fernandez</a>
+ * @version 1.0
+ * @since <pre>feb. 17, 2022</pre>
  */
 public class ReusablePoolTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@Before
-	public void setUp() throws Exception {
-	}
+    private static ReusablePool pool;
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-	}
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        pool = ReusablePool.getInstance();
+    }
 
-	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#getInstance()}.
-	 */
-	@Test
-	public void testGetInstance() {
-		System.out.print("To be implemented");
-	}
+    /**
+     * @throws java.lang.Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
-	 */
-	@Test
-	public void testAcquireReusable() {
-		System.out.print("To be implemented");
-	}
+    /**
+     * Test method for {@link ubu.gii.dass.c01.ReusablePool#getInstance()}.
+     */
+    @Test
+    public void testGetInstance() {
+        System.out.print("To be implemented");
+    }
 
-	/**
-	 * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
-	 */
-	@Test
-	public void testReleaseReusable() {
-		System.out.print("To be implemented");
-		System.out.print("To be implemented");
+    /**
+     * Test method for {@link ubu.gii.dass.c01.ReusablePool#acquireReusable()}.
+     */
+    @Test
+    public void testAcquireReusable() {
+        System.out.print("To be implemented");
+    }
 
-	}
+    /**
+     * Test method for {@link ubu.gii.dass.c01.ReusablePool#releaseReusable(ubu.gii.dass.c01.Reusable)}.
+     */
+    @Test
+    public void testReleaseReusable() {
+        try {
+            Reusable r = new Reusable();
+            pool.releaseReusable(r);
+            try {
+                pool.releaseReusable(r);
+            } catch (DuplicatedInstanceException e) {
+                assertTrue(true);
+            } catch (Exception ex) {
+                System.err.println(ex);
+                assertTrue("another exception has been raised", false);
+            }
+
+        } catch (DuplicatedInstanceException ex1) {
+            assertTrue("Empty pool raises DuplicatedInstanceException", false);
+        } catch (Exception ex2) {
+            System.err.println(ex2);
+            assertTrue("Empty pool raises Exception", false);
+        }
+
+
+    }
 
 }
